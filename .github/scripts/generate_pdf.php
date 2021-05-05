0d8ec1c020b610878e5e3e4b0efb4d8554a7c143
Oleg Andreyev <oleg@andreyev.lv>
2021-05-06 00:02:18 +0300

    updated ci

--- .github/scripts/generate_pdf.php
+++ .github/scripts/generate_pdf.php
@@ -6,5 +6,10 @@ use Knp\Snappy\Pdf;
 use OAndreyev\WKHTMLToPDF\WKHTMLToPDF;
 
 $snappy = new Pdf();
-$snappy->setBinary(WKHTMLToPDF::PATH);
+$arch = php_uname('m');
+if ($arch === 'x86_64') {
+    $snappy->setBinary(WKHTMLToPDF::PATH_AMD64);
+} else {
+    $snappy->setBinary(WKHTMLToPDF::PATH_I368);
+}
 echo $snappy->getOutputFromHtml('<p>foobar</p>');
